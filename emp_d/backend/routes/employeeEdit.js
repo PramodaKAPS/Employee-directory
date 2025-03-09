3f0e067cc83fa0b5db1536f3143a32395debe7e8
Pramoda Katipearachchi <121634558+PramodaKAPS@users.noreply.github.com>
2025-03-09 19:09:54 +0530

    Update employeeEdit.js

--- emp_d/backend/routes/employeeEdit.js
+++ emp_d/backend/routes/employeeEdit.js
@@ -2,45 +2,43 @@ const express = require('express');
 const multer = require('multer');
 const path = require('path');
 const fs = require('fs');
-const pool = require('../database'); // Ensure this path correctly leads to your database configuration
+const pool = require('../database'); 
 const router = express.Router();
 
-// Setup Multer for file uploads
 const storage = multer.diskStorage({
   destination: function(req, file, cb) {
     cb(null, 'uploads/');
   },
   filename: function(req, file, cb) {
-    cb(null, Date.now() + path.extname(file.originalname)); // Append the date to the original filename
+    cb(null, Date.now() + path.extname(file.originalname)); 
   }
 });
 
 const upload = multer({ storage: storage });
 
-// Endpoint to update an employee's details
+
 router.put('/:id', upload.single('photo'), (req, res) => {
   const { id } = req.params;
   const { first_name, last_name, gender, maritalStatus, dob, email, mobile, address, department, position } = req.body;
-  let photoFilename = req.file ? req.file.filename : undefined; // Use undefined to signify no new photo
+  let photoFilename = req.file ? req.file.filename : undefined; 
 
-  // Initialize the query and values array with everything except the photo
   let query = `
     UPDATE employees
     SET first_name = ?, last_name = ?, gender = ?, marital_status = ?, dob = ?, email = ?, mobile = ?, address = ?, department = ?, position = ?`;
   let values = [first_name, last_name, gender, maritalStatus, dob, email, mobile, address, department, position];
 
-  // If a new photo was uploaded, append it to the query and values
+  
   if (photoFilename !== undefined) {
     query += `, photo = ?`;
     values.push(photoFilename);
   }
 
-  // Finish preparing the query and values
+  
   query += ` WHERE id = ?`;
   values.push(id);
 
-  // Convert empty dob string to null
-  values = values.map(value => value === '' ? null : value); // General handling for empty strings to null
+  
+  values = values.map(value => value === '' ? null : value); 
 
   pool.query(query, values, (error, results) => {
     if (error) {
